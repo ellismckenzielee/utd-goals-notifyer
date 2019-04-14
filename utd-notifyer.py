7c59d46234798403d52f8f82b71267f4509e13a9
Ellis Lee <ellismckenzielee@gmail.com>
2019-04-14 22:42:02 +0100

    Added requests and beautiful soup dependencies, checked for errors

--- utd-notifyer.py
+++ utd-notifyer.py
@@ -1,2 +1,8 @@
 #Script periodically checks for goals on the BBC website 
 #Pushes a notification to Linux desktop
+
+import requests, bs4
+
+res = requests.get('https://www.bbc.co.uk/sport/football')
+
+res.raise_for_status()
