7086666180f40cee080951ded65b117c2b4f0ea2
Ellis Lee <ellismckenzielee@gmail.com>
2019-04-14 23:31:04 +0100

    Changed BBC to GOAL and also found the class which can be used to obtain scores

--- utd-notifyer.py
+++ utd-notifyer.py
@@ -1,8 +1,13 @@
-#Script periodically checks for goals on the BBC website 
+#Script periodically checks for goals on the GOAL website 
 #Pushes a notification to Linux desktop
 
 import requests, bs4
 
-res = requests.get('https://www.bbc.co.uk/sport/football')
+url = 'https://www.goal.com/en-gb/team/manchester-united/fixtures-results/6eqit8ye8aomdsrrq0hk3v7gh'
 
+res = requests.get(url)
 res.raise_for_status()
+
+soupified_GOAL = bs4.BeautifulSoup(res.text)
+goals = soupified_GOAL.select('.goals')
+print(goals)
