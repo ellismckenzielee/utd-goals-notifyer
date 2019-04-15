8b1d4d426b93912b6eb5acd4d4b3ce85d9beadab
Ellis Lee <ellismckenzielee@gmail.com>
2019-04-15 19:37:26 +0100

    used beautiful soup to get a list of: goals, teams and match status. Using the latter, code determines which game is currently on, and then it finds the teams involved and goals for each

--- utd-notifyer.py
+++ utd-notifyer.py
@@ -8,6 +8,28 @@ url = 'https://www.goal.com/en-gb/team/manchester-united/fixtures-results/6eqit8
 res = requests.get(url)
 res.raise_for_status()
 
-soupified_GOAL = bs4.BeautifulSoup(res.text)
-goals = soupified_GOAL.select('.goals')
-print(goals)
+soupified = bs4.BeautifulSoup(res.text)
+goals = soupified.find_all(class_="goals")
+teams = soupified.find_all(class_="team-name")
+match_status = soupified.find_all(class_='match-status')
+
+def match_finder(match_status):
+    '''Finds the current game based on which games have FT'''
+    match_count = 0
+    for match in match_status:
+        full_time = match.span
+        if full_time != None:
+            match_count += 1
+            if full_time.string == None:
+                return match_count - 1
+        else: 
+            pass
+
+
+match_number = match_finder(match_status)
+
+print(teams[match_number*2].string)
+print(teams[(match_number*2)+1].string)
+
+print(goals[match_number*2].string)
+print(goals[match_number*2].string)
