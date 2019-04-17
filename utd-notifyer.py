37274201afe02e3bb0e5e5b552473f3704ab143b
Ellis Lee <ellismckenzielee@gmail.com>
2019-04-17 19:09:11 +0100

    Refactored the code by placing all into a function, and put this within a while loop which sleeps every 60 seconds (checks for a goal every minute). Also, the match count contained a bug (full_time.string == None), as it was realized that when a game was live this would not be the case

--- utd-notifyer.py
+++ utd-notifyer.py
@@ -1,35 +1,39 @@
 #Script periodically checks for goals on the GOAL website 
 #Pushes a notification to Linux desktop
 
-import requests, bs4
+import requests, bs4, time
 
 url = 'https://www.goal.com/en-gb/team/manchester-united/fixtures-results/6eqit8ye8aomdsrrq0hk3v7gh'
-
-res = requests.get(url)
-res.raise_for_status()
-
-soupified = bs4.BeautifulSoup(res.text)
-goals = soupified.find_all(class_="goals")
-teams = soupified.find_all(class_="team-name")
-match_status = soupified.find_all(class_='match-status')
 
 def match_finder(match_status):
     '''Finds the current game based on which games have FT'''
     match_count = 0
     for match in match_status:
         full_time = match.span
+        print('Fulltime',full_time)
         if full_time != None:
             match_count += 1
-            if full_time.string == None:
+            if full_time.string != 'FT':
                 return match_count - 1
-        else: 
-            pass
 
 
-match_number = match_finder(match_status)
+def get_data():
+    res = requests.get(url)
+    res.raise_for_status()
 
-print(teams[match_number*2].string)
-print(teams[(match_number*2)+1].string)
+    soupified = bs4.BeautifulSoup(res.text)
+    goals = soupified.find_all(class_="goals")
+    teams = soupified.find_all(class_="team-name")
+    match_status = soupified.find_all(class_='match-status')
+    match_number = match_finder(match_status)
+    print(match_number)
+    print(teams[match_number*2].string)
+    print(teams[(match_number*2)+1].string)
 
-print(goals[match_number*2].string)
-print(goals[match_number*2].string)
+    print(goals[match_number*2].string)
+    print(goals[match_number*2].string)
+
+###MAIN LOOP
+while True:
+    get_data()
+    time.sleep(60)
